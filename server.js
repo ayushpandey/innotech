2fe0774931c84e51085be0622d93e9bdcfa5874b
Ayush Pandey <Ayush Pandey>
2017-01-19 23:54:07 +0530

    server mods

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 http = require('http');
 url = require('url');
+
 http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.end("It's working");
+	var requestURL = url.parse(req.url, true)['pathname'];
+	console.log("Created server");
+	if (requestURL == '/register') {
+		res.writeHead(200, {'Content-Type': 'text/plain' });
+		res.end("It's registering");
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/plain' });
+		res.end("It's working");
+	}
 }).listen(process.env.PORT || 3000);
-console.log("listening on port 4000");
+console.log("listening on port 3000");
