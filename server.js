902a67a06c6e15e02dd07ed3f86db5ad7d22844b
Ayush Pandey <Ayush Pandey>
2017-01-19 23:45:43 +0530

    server modified to send a registration token

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 http = require('https');
 url = require('url');
-
 http.createServer(function(req, res){
-	var requestURL = url.parse(req.url, true)['pathname'];
-	console.log("Created server");
 	res.writeHead(200, {'Content-Type': 'text/plain' });
 	res.send("It's working");
 }).listen(process.env.PORT || 3000);
