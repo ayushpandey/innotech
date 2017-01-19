db7dc2f7a848395e2cc577352722faf0a15aabb9
Ayush Pandey <Ayush Pandey>
2017-01-19 23:44:07 +0530

    server modified to send a registration token

--- server.js
+++ server.js
@@ -4,12 +4,7 @@ url = require('url');
 http.createServer(function(req, res){
 	var requestURL = url.parse(req.url, true)['pathname'];
 	console.log("Created server");
-	if (requestURL == '/l') {
-		res.writeHead(200, {'Content-Type': 'text/plain' });
-		res.send("It's working");
-	} else {
-		res.writeHead(200, {'Content-Type': 'text/plain' });
-		res.send("It's working");
-	}
+	res.writeHead(200, {'Content-Type': 'text/plain' });
+	res.send("It's working");
 }).listen(process.env.PORT || 3000);
 console.log("listening on port 3000");
