2807709304428aed642a4b6488b400ad5468ba86
Ayush Pandey <Ayush Pandey>
2017-01-19 23:46:26 +0530

    server modified to send a registration token

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain' });
 	res.send("It's working");
 }).listen(process.env.PORT || 3000);
-console.log("listening on port 3000");
+console.log("listening on port 4000");
