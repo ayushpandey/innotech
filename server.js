478628ad088f158cbd7933eef774c1ddd12bf49a
Ayush Pandey <Ayush Pandey>
2017-01-19 23:52:34 +0530

    server mods

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-http = require('https');
+http = require('http');
 url = require('url');
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.send("It's working");
+	res.end("It's working");
 }).listen(process.env.PORT || 3000);
 console.log("listening on port 4000");
