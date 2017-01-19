bdc3e9091487d541a023409afd020ca60c8871fa
Ayush Pandey <Ayush Pandey>
2017-01-19 23:31:44 +0530

    server modified to send a registration token

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-http = require('http');
+http = require('https');
 url = require('url');
 
 http.createServer(function(req, res){
 	var requestURL = url.parse(req.url, true)['pathname'];
-
-	if (requestURL == '/log.gif') {
-		var imgHex = '47494638396101000100800000dbdfef00000021f90401000000002c00000000010001000002024401003b';
-		var imgBinary = new Buffer(imgHex, 'hex');
-		res.writeHead(200, {'Content-Type': 'image/gif' });
-		res.end("<img src='https://emailtrack.herokuapp.com/log.gif' width='1 px' height='1 px'>");
-
-		console.log(req);
-
+	console.log("Created server");
+	if (requestURL == '/register') {
+		res.writeHead(200, {'Content-Type': 'text/plain' });
+		res.end("It's working");
 	} else {
 		res.writeHead(200, {'Content-Type': 'text/plain' });
 		res.end("It's working");
 	}
 }).listen(process.env.PORT || 3000);
+console.log("listening on port 3000");
