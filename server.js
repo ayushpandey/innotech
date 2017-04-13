98a7aab52f0a477f4b646f31a5b7f8b4f27dd78f
Ayush Pandey <ayushpandey8439@gmail.com>
2017-04-13 22:37:19 +0530

    Timestamp formatting

--- server.js
+++ server.js
@@ -31,7 +31,9 @@ app.get('/register', function(req,res){
 app.post('/email', function(req,res){
   console.log("Got something");
   var email_data = req.body;
+  email_data.timestamp = (email_data.timestamp.substr(0,email_data.timestamp.lastIndexOf(":"))).trim();
   var log = new trackingLog (email_data);
+  console.log(email_data);
   log.save(function(err, logResponse,num){
     if(err) console.log(err);
     else {
@@ -44,13 +46,23 @@ app.post('/email', function(req,res){
 app.post('/read', function(req,res){
   console.log("Access on /read");
   var read_data = req.body;
+
+  var ts = read_data.timestamp.split(',')
+  timestamp = "/^"+ts[0]+" "+ts[1];
   var query = {
     sender : read_data.sender,
     recipients : read_data.recipients,
-    subject : read_data.subject,
+    timestamp : {$regex: timestamp}
   }
-  trackingLog.find(query).find(function(data){
-    console.log(data);
+  trackingLog.find(query,function(err,data){
+    console.log(query);
+    if(err){
+      console.log(err);
+      res.end(err.toString());
+    } 
+    else {
+      res.end(data.toString());
+    }
   }); 
 });
 
