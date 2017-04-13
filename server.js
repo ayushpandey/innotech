adf2459a89c5808c6cdd66c3250c5b3b6dd1cf54
Ayush Pandey <ayushpandey8439@gmail.com>
2017-04-14 00:32:16 +0530

    integrated FCM

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('https');
 var mongoose = require('mongoose');
+
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: "emailtracker-15c78",
+    clientEmail: "firebase-adminsdk-l1jla@emailtracker-15c78.iam.gserviceaccount.com",
+    privateKey: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC8CF7DyiFkNquj\nRfd94BI4rJTHoECPmnHf2V0BpO0X58dwGYEAOuXsaZKnpogsBImVcYIFubvzQR5G\n3Idxm0i4mBP0AACg9EBdESDHk2sguKoz5vO60G9wcF0d/N6yk9hHAdo5XCLs4HB9\nZsav1++Yh97O0u0HUz3F9JxQbKhg8Vviha8pUk9P1El07dRlzeNEHMp/KzQJDl42\npS2PbND1d+bFRbk6ay/Mo6C6uucD8Gl4dlAt1QdJqpGAAr4xtz+y0K8/ADH2ZaNK\nRD6O/9JHWlIugtGdNxsyQ+GSTRIxhzg6BxKqpZNuNwr8YkZt91QgWW2675F2Z5gj\nFFK3uct7AgMBAAECggEARRo6YW/mZ6Lj3yQ2eU8gwy1BnD+bmmrjEKYcVR9cdt2a\nYOsuinwaxZBi8jP0dWDRg1dstlf058f1rYscR7cOubBCHPjnQj+ha9KAnnfh9euH\nuX1V80Dow4S34+5rlWtaH3Zh0ZS3zSAKlztvJ26PQhok2xOPQpLn79kenTvhgYZv\nrWCdCipfBky0eJCaA0snsTEQ5/bO3trMRrZx+eIawOlNQrbioTV66+IwFxYCvpQb\nrBRFvRe+uRUX/NLN/bGEVHdCwAOMEOTrNzc+cS4UAX4gpm3Doet9pO0O3roWKrAa\n2Jg1Lw2fzOulZ/0uGJJpnUogSrtQWminwP9W6mLKUQKBgQD2Nk35UteAjQMGUvvd\nbDaU6YiFKqTxLynPr1RgcxIuK5xUNE3PUamzLcBlfqsAsDvUq85fRKkZchOQeKT5\nKNKZXugRVRJGvsn6TAPLfwggy1gngi8II1rw8bakrVQcBWXeHXzgBkvAogjzCU1q\n1GVDKpb7w+EOX1VIqNmocWHZYwKBgQDDgfmMHwooxWR8vTEge39GJQA313Sut9HW\ncGm9fkSKwZEEy9SURSnR5LgZXh3N5Sz+qZoiyfhkEhKbsUtvw3APSh+pif0YoV3y\nZVEGFBjCGiVIkRHwh8hg4YKZst4lTcodKvk1rdryQ6qxsQw55H/xGOxRPWSjXN9K\nF5yykX5tCQKBgQCLnofhFuU19MD+WRiSm8bn8vaD7YbTbldbeqVyfE6W+AwShAaL\nxldhUFSGOapnHYkQNM02oQzzbQlDJ+cFk+y5JGSdT4ypqY+H7ItKXqMRVb6LyPs6\ntjHf6HIWOGQ176mwpzAlKezTNA+3IlNQfVlfGUu9QXriV5VZOscTChje2QKBgQCZ\nPHuCxyVoVWbwmj0yL3nPWdr06cvL0coHoK5QVvKUKylmL/jbieZAXFurjmgf5DvD\nEt373H/yt3OcUndFD1L6uOdeNAiLUJdZEO5TrFbHdES6vvHnrBVzUe3SWC7j4DfF\natRPrYfJkR3uDk395kodh6JZ/putq3JcGWu5Tm5gOQKBgHGUSuSYFogQUPzjtepX\nYhFFfOyd576YFeKGR4dpZD9/2S5SyhYwqSCmyEuQuDilYi6ANLILferEWUep4qQE\nZnrRskjdpwpQFg6aqIKc4vyMCetUrTkptD/rfmN/nJ00TuI+HPWp1UIsCjzE+D3v\nAUCEuV4Tv37EYUkyoiwFxn/3\n-----END PRIVATE KEY-----\n"
+  }),
+  databaseURL: "https://emailtracker-15c78.firebaseio.com/"
+});
 var app = express();
 
 mongoose.connect(process.env.MONGODB_URI, function (error) {
@@ -14,7 +25,8 @@ Schema = new mongoose.Schema({
       sender    : String,
       recipients : Array,
       subject : String,
-      timestamp : String
+      timestamp : String,
+      fcmID: String
     }),
 
 trackingLog = mongoose.model('trackingLog',Schema);
@@ -56,6 +68,7 @@ app.post('/email', function(req,res){
     }
   });
   //writeUserData(req.body.msgID, req.body.email);
+  res.end();
 });
 
 app.post('/read', function(req,res){
@@ -68,12 +81,41 @@ app.post('/read', function(req,res){
     timestamp : ts
   }
   trackingLog.find(query,function(err,data){
-    console.log(query);
     if(err){
       console.log(err);
       res.end(err.toString());
     } 
     else {
+                  // This registration token comes from the client FCM SDKs.
+                  console.log(data[0].fcmID);
+                  var registrationToken = data[0].fcmID; 
+                  var date = Date.now();
+                  date = new Date(date);
+                  date = date.toString();
+                  // See the "Defining the message payload" section below for details
+                  // on how to define a message payload.
+                  var payload = {
+                    data: {
+                      readBy: read_data.recipients,
+                      timestamp: date
+                    }
+                  };
+
+                  // Send a message to the device corresponding to the provided
+                  // registration token.
+                  admin.messaging().sendToDevice(registrationToken, payload)
+                    .then(function(response) {
+                      // See the MessagingDevicesResponse reference documentation for
+                      // the contents of response.
+                      console.log("Successfully sent message:", response);
+                    })
+                    .catch(function(error) {
+                      console.log("Error sending message:", error);
+                    });
+
+
+
+      console.log(data);
       res.end(data.toString());
     }
   }); 
