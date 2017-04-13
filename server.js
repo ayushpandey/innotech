f893e5fdf88ce3e3bb514ccc9a9a83d35318c1d7
Ayush Pandey <ayushpandey8439@gmail.com>
2017-04-13 23:36:04 +0530

    Server reads

--- server.js
+++ server.js
@@ -32,12 +32,25 @@ app.post('/email', function(req,res){
   console.log("Got something");
   var email_data = req.body;
   email_data.timestamp = (email_data.timestamp.substr(0,email_data.timestamp.lastIndexOf(":"))).trim();
+  var lastSpace = email_data.timestamp.lastIndexOf(' ');
+  var colon = email_data.timestamp.indexOf(':');
+  console.log(lastSpace);
+  var hr = Number(email_data.timestamp.substr(lastSpace+1,2));
+  console.log(typeof(hr));
+  if(hr>=13) {
+    hr = hr-12;
+    time = " PM"
+  }
+  else if( hr < 13) {
+    time = " AM"
+  }
+  timestamp = email_data.timestamp.substring(0,lastSpace+1) + hr.toString() + email_data.timestamp.substring(colon)+ time;
+  console.log(timestamp);
   var log = new trackingLog (email_data);
-  console.log(email_data);
   log.save(function(err, logResponse,num){
     if(err) console.log(err);
     else {
-      console.log(logResponse, num);
+      //console.log(logResponse, num);
     }
   });
   //writeUserData(req.body.msgID, req.body.email);
@@ -46,13 +59,12 @@ app.post('/email', function(req,res){
 app.post('/read', function(req,res){
   console.log("Access on /read");
   var read_data = req.body;
-
-  var ts = read_data.timestamp.split(',')
-  timestamp = "/^"+ts[0]+" "+ts[1];
+  var ts = read_data.timestamp.replace(/,/g,'').replace(/ at /g, ' ');
+  console.log(ts);
   var query = {
     sender : read_data.sender,
     recipients : read_data.recipients,
-    timestamp : {$regex: timestamp}
+    timestamp : ts
   }
   trackingLog.find(query,function(err,data){
     console.log(query);
